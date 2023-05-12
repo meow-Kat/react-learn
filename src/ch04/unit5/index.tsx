a42c2fa3d8351a1742e0e2a9882b1a1732d8409d
meow-Kat <76527080+meow-Kat@users.noreply.github.com>
2023-05-12 21:57:10 +0800

    update

--- src/ch04/unit5/index.tsx
+++ src/ch04/unit5/index.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect,useState } from 'react';
 
+// 初始化的全域變數
 let interval: NodeJS.Timeout | null  = null
 let num = 0
 
 const App:React.FC = () => {
 
   const [counter, setCounter] = useState(0)
-
   useEffect(() => {
     interval = setInterval(() => {
       num++
@@ -14,7 +14,7 @@ const App:React.FC = () => {
       
     }, 1000)
 
-    // callback function
+    // 要把每次點擊之後的 setInterval 消失不然會一直疊上去
     return () => {
       if(interval !== null) clearInterval(interval)
       num = 0
@@ -28,7 +28,7 @@ const App:React.FC = () => {
 
   return <>
     <h1>計時器: {counter}</h1>
-    {/* 按了很多次會一直執行疊加上去 */}
+    {/* 按了很多次會一直執行疊加上去會跑多次的 setCounter 再重新渲染 setInterval 也就是按了幾次按鈕就會產生 setInterval */}
     <button onClick={clickHandler}>+1</button>
   </>
 }
